9e5d39d6930951df8ad64e8eb85f180685f5b2ce
Robert Ingrum <rkingrum@gmail.com>
2017-10-16 23:11:09 -0600

    Fix env config

--- spec/pretty_scssy/aligner_spec.rb
+++ spec/pretty_scssy/aligner_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
-RSpec.describe PrettyScssy do
-  describe PrettyScssy::Aligner do
-    it 'should run this test' do
+RSpec.describe PrettyScssy::Aligner do
+  context '.run' do
+    it 'should create an instance of itself and then run it' do
+      expect(described_class).to receive(:run).and_call_original
+      expect_any_instance_of(described_class).to receive(:run)
+
+      described_class.run
     end
   end
 end
